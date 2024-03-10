34a138eba6d4ea29fc398d02e5226890fa8b0675
currydev <kahiki@hotmail.de>
2024-03-10 19:05:52 +0100

    spring init

---
+++ src/main/java/com/example/buyroll/BuyrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buyroll/BuyrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buyroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
